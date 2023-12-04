2d01009438d04e1949d65e1ee900870d0ba11c7c
jerryuhoo <jerryuhoo@gmail.com>
2023-12-03 22:07:49 -0500

    update seg cat

--- seg_cat.py
+++ seg_cat.py
@@ -1,14 +1,23 @@
 import librosa
 import librosa.display
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import soundfile as sf
+from scipy.signal import butter, filtfilt
 
 
-def load_audio(filename):
-    y, sr = librosa.load(filename, sr=None)
-    return y, sr
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype="high", analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+
+def load_and_filter_audio(filename, sr=16000, cutoff=400):
+    y, sr = librosa.load(filename, sr=sr)
+    y_filtered = butter_highpass_filter(y, cutoff, sr)
+    return y_filtered, sr
 
 
 def calculate_energy(y, window_size, step_size):
@@ -20,7 +29,7 @@ def calculate_energy(y, window_size, step_size):
 
 
 def find_and_save_high_energy_segments(
-    y, sr, energy, threshold, base_filename, window_size
+    y, sr, energy, threshold, base_filename, window_size, output_folder
 ):
     current_segment_start = None
     current_segment_end = None
@@ -35,8 +44,8 @@ def find_and_save_high_energy_segments(
             if current_segment_start is not None:
                 segment_audio = y[current_segment_start:current_segment_end]
                 segment_filename = f"{base_filename}_seg_{segment_index}.wav"
-                print(segment_filename)
-                sf.write(segment_filename, segment_audio, sr)
+                output_file_path = os.path.join(output_folder, segment_filename)
+                sf.write(output_file_path, segment_audio, sr)
                 segment_index += 1
 
                 current_segment_start = None
@@ -45,18 +54,27 @@ def find_and_save_high_energy_segments(
     if current_segment_start is not None:
         segment_audio = y[current_segment_start:current_segment_end]
         segment_filename = f"{base_filename}_seg_{segment_index}.wav"
-        print(segment_filename)
-        sf.write(segment_filename, segment_audio, sr)
+        output_file_path = os.path.join(output_folder, segment_filename)
+        sf.write(output_file_path, segment_audio, sr)
 
 
-sr = 16000
-filename = "/home/yyu479/ddsp-pytorch/data/cats/train/cat_2.wav"
+folder_path = "/home/yifeng/SVS/ddsp_pytorch/data/cats/train"
+output_folder = "/home/yifeng/SVS/ddsp_pytorch/data/cats/train2"
 threshold = 100
-window_size = 3 * sr
+sr = 16000
+window_size = sr // 2
 step_size = window_size // 2
-save_path = "spectrogram.png"
-base_filename, _ = os.path.splitext(filename)
 
-y, sr = load_audio(filename)
-energy = calculate_energy(y, window_size, step_size)
-find_and_save_high_energy_segments(y, sr, energy, threshold, base_filename, window_size)
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(folder_path):
+    if filename.endswith(".wav"):
+        full_path = os.path.join(folder_path, filename)
+        base_filename, _ = os.path.splitext(filename)
+
+        y, sr = load_and_filter_audio(full_path, sr)
+        energy = calculate_energy(y, window_size, step_size)
+        find_and_save_high_energy_segments(
+            y, sr, energy, threshold, base_filename, window_size, output_folder
+        )
