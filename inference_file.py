8d43455d9a39d76ac67a631c96a9a0d936e75f4b
jerryuhoo <jerryuhoo@gmail.com>
2023-12-07 23:27:36 -0500

    update inference

--- inference_file.py
+++ inference_file.py
@@ -1,29 +1,40 @@
 import torch
-from preprocess import preprocess
-import librosa as li
 import numpy as np
+from scipy.io.wavfile import write
+from preprocess import preprocess
+import argparse
 
-model = torch.jit.load("export/ddsp_mytraining_pretrained.ts")
+# Set up command line arguments
+parser = argparse.ArgumentParser(description="Process model and input file paths.")
+parser.add_argument(
+    "--model_path",
+    type=str,
+    default="export/ddsp_cats_seg_autotune_pretrained.ts",
+    help="Path to the model file.",
+)
+parser.add_argument(
+    "--input_file",
+    type=str,
+    default="violin_original.mp3",
+    help="Path to the input audio file.",
+)
+args = parser.parse_args()
 
-f = "violin_original.mp3"
+# Load the model
+model = torch.jit.load(args.model_path)
 
-x, pitch, loudness = preprocess(f, 16000, 160, 64000, False)
-print("pitch", pitch.shape)
-print("loudness", loudness.shape)
+# Preprocess input file
+x, pitch, loudness = preprocess(args.input_file, 16000, 160, 64000, False)
 pitch = pitch[0]
 loudness = loudness[0]
 pitch = torch.tensor(pitch).unsqueeze(0).unsqueeze(-1).float()
-print("pitch", pitch.shape)
 loudness = torch.tensor(loudness).unsqueeze(0).unsqueeze(-1).float()
-print("loudness", loudness.shape)
 
-
+# Generate audio
 audio = model(pitch, loudness)
 
-# save audio
-from scipy.io.wavfile import write
-
+# Save audio
 audio_numpy = audio[0].detach().cpu().numpy()
 # normalized_audio = audio_numpy / np.max(np.abs(audio_numpy))
 audio_16bit = np.int16(audio_numpy * 32767)
-write("test_output1.wav", 16000, audio_16bit)
+write("test_output.wav", 16000, audio_16bit)
